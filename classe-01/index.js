7ab8d06ec3bc8356ff21213c63d832d063811097
JeanSisse <tavares1988@live.com>
2021-06-27 20:38:13 -0300

    Exercicio 'Sistema de filtro de comentários' resolvido

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,12 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+function verificar_comentario(comentario) {
+	
+	if (comentario.toLowerCase().includes("covid") || comentario.toLowerCase().includes("pandemia")) {
+		console.log("Comentario bloqueado por conter palavras proibidas.");
+	} else {
+		console.log("Comentário autorizado");
+	}
+}
+
+verificar_comentario(comentario);
