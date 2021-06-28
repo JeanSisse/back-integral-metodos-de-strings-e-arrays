08d883c7679a84bc938992f8b20ad7f8c31e61dd
JeanSisse <tavares1988@live.com>
2021-06-27 21:55:29 -0300

    Exercicio 'URL amigavel' resolvido

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,11 @@
-const texto = "Aprenda programar do zero na Cubos Academy";
+const texto = 'Aprenda programar do zero na Cubos Academy';
+
+function transformar_para_url_amigavel(texto){
+	let urlAmigavel = texto.trim();
+
+	urlAmigavel = urlAmigavel.split(" ").join("-");
+
+	return urlAmigavel;
+}
+
+console.log(transformar_para_url_amigavel(texto));
