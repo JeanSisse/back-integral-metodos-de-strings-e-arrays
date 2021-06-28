1b8c2f22b2c54e0bb9cf31614367911d8454dc3b
JeanSisse <tavares1988@live.com>
2021-06-28 12:33:10 -0300

    Exercicio 'Sistema para biblioteca' resolvido

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,2 +1,18 @@
 const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
 const nomeDoLivro = "Dom Quixote";
+
+
+function localiza_livro(livros, nomeDoLivro){
+	let index = livros.indexOf(nomeDoLivro);
+	let msg = "";
+
+	if(index != -1){
+		msg = `O livro esta na posição ${index + 1}`;
+	} else {
+		msg = "O livro não existe";
+	}
+
+	console.log(msg);
+}
+
+localiza_livro(livros, nomeDoLivro);
